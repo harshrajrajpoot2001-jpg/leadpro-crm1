6b26cee250b31ef9c7b3bb36f7dab4c51c25c8a1
harshrajrajpoot2001-jpg <harshrajrajpoot2001@gmail.com>
2026-06-08 03:28:37 +0530

    Create crmDb.ts

---
+++ src/crmDb.ts
@@ -0,0 +1,11 @@
+export const subscribeCustomers = () => {};
+export const addCustomer = () => {};
+export const updateCustomer = () => {};
+export const deleteCustomer = () => {};
+export const seedInitialCustomers = () => {};
+
+export const subscribeFollowUps = () => {};
+export const addFollowUp = () => {};
+export const updateFollowUp = () => {};
+export const deleteFollowUp = () => {};
+export const seedInitialFollowUps = () => {};
